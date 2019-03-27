a9eabdf704dc5f337d92787eec188b0a466eb807
Thai Nguyen <thaima.uit@gmail.com>
2019-03-27 15:10:53 +0700

    Updates

---
+++ precache-manifest.1aa304568299073d982c75cae618f711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ffe0a71ae799e23d250e",
+    "url": "/static/js/main.833af6e5.chunk.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "ffe0a71ae799e23d250e",
+    "url": "/static/css/main.73d4e1cd.chunk.css"
+  },
+  {
+    "revision": "ec72bc643e50125544b72a1f1be60ad7",
+    "url": "/index.html"
+  }
+];
